9bcd7c127410ca5b8b3995798be260fa725aa96e
vishnu5002 <110230643+vishnu5002@users.noreply.github.com>
2022-11-09 09:49:54 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,24 +1,39 @@
+""" linked list implementation of stack """
+
 class Node:
   def __init__(self, data):
     self.data = data
     self.next = None
-
+    
 
 class Stack:
   def __init__(self):
     self.head = None
 
   def push(self, data) -> None:
-    # Write your code here
+    new = Node(data)
+    if not self.head== None:
+      new.next = self.head
+      self.head=new
+    else:
+      self.head=new
+      
 
   def pop(self) -> None:
-    # Write your code here
+    if not self.head==None:
+      self.head = self.head.next
 
   def status(self):
     """
     It prints all the elements of stack.
     """
     # Write your code here  
+    ele= ""
+    temp=self.head
+    while temp is not None:
+      ele += str(temp.data) + "=>"
+      temp=temp.next
+    print (ele+"None")
 
 
 # Do not change the following code
